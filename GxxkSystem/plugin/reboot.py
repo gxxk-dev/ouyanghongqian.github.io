cfad3b7ddefe7af02a80646835826f2c4676be94
ouyanghongqian <ouyanghongqian@qq.com>
2023-01-13 14:39:38 +0800

    Site updated: 2023-01-13 14:39:33

---
+++ GxxkSystem/plugin/reboot.py
@@ -0,0 +1,10 @@
+import time
+import machine
+from mpython import *
+def start():
+    oled.fill(0)
+    oled.DispChar("将在2s后重启...",0,0)
+    oled.DispChar("重启后长按B可进入REPL",0,16,auto_return=True)
+    oled.show()
+    time.sleep(2)
+    machine.reset()
